e9da2380ec7f63b427118f75985013b7c7125d06
wanlay <wlwanlay@gmail.com>
2018-08-13 20:30:23 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:wanlay/document.git master:gh-pages
+git push -f "https://${GH_TOKEN}@github.com/wanlay/document" master:gh-pages
 
 cd -
